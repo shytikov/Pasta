01975fbbdb1f192fe08a9c232b0be2d61a0fa851
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-10-21 13:48:55 +0300

    Avoiding usage of additional base class

--- FrontEnd/Controllers/PastieController.cs
+++ FrontEnd/Controllers/PastieController.cs
@@ -4,21 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FrontEnd.Models;
+using Raven.Client;
+using FrontEnd.Warehouse;
 
 namespace FrontEnd.Controllers
 {
     public class PastieController : Controller
     {
+        #region RavenDB's specifics
+
+        public IDocumentSession DocumentSession { get; set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.IsChildAction)
+            {
+                return;
+            }
+
+            this.DocumentSession = Storage.Instance.OpenSession();
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.IsChildAction)
+            {
+                return;
+            }
+
+            if (this.DocumentSession != null && filterContext.Exception == null)
+            {
+                this.DocumentSession.SaveChanges();
+            }
+
+            this.DocumentSession.Dispose();
+            base.OnActionExecuted(filterContext);
+        }
+        #endregion
+
         //
-        // GET: /Pastie/
+        // GET: /Pastie/Create
 
         public ActionResult Create()
         {
-            return View();
+            var pastie = new Pastie();
+            return View(pastie);
         }
 
         //
-        // POST: /Pastie/
+        // POST: /Pastie/Create
 
         [HttpPost]
         public ActionResult Create(Pastie pastie)
