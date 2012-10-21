8c61eee1b82f86343909e92cae6e1fc8d4ca78cf
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-10-21 09:42:51 +0300

    Changing entity name from 'Patch' to 'Pastie'

--- FrontEnd/Logic/Pastie.cs
+++ FrontEnd/Logic/Pastie.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace FrontEnd.Logic
 {
-    public class Patch
+    public class Pastie
     {
         public string Id { get; set; }
         public string Content { get; set; }
