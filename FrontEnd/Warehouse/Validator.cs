94e20ebba2a2347363baa8d84e9e2af692e53255
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-10-22 00:13:35 +0300

    Trying to use custom validator instead of configuration setting forbidding request validation

---
+++ FrontEnd/Warehouse/Validator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Util;
+
+namespace FrontEnd.Warehouse
+{
+    public class Validator : RequestValidator
+    {
+    }
+}
