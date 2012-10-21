454146d43fced0da98273f5f53a5bd36248380fd
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-10-21 11:23:11 +0300

    Re-routing application to land on Pastie/Create view by default

---
+++ FrontEnd/Controllers/PastieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class PastieController : Controller
+    {
+        //
+        // GET: /Pastie/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
