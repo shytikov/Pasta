53851370d02f773a080d9a3ce99ffe7a0acc8687
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-10-21 10:59:55 +0300

    Adding model wireframe to handle pasties

---
+++ FrontEnd/Models/Pasties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Models
+{
+    public class Pasties : Controller
+    {
+        //
+        // GET: /Pastie/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
