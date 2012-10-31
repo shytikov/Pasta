2f9b1869101683cd2fbfff87c86b98776d97f1ba
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-10-31 18:28:35 +0200

    continue with Nancy migration

--- Basta/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Basta.MvcApplication" Language="C#" %>
---
+++ Basta/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Basta
+{
+    public class MainModule : NancyModule
+    {
+        public void HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
