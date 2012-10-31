fd7137f745039977175c46e08d7dd5ad96c21f95
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-10-31 13:48:47 +0200

    Updating copyright information

--- Basta/Properties/AssemblyInfo.cs
+++ Basta/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FrontEnd")]
+[assembly: AssemblyTitle("Basta")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EPAM Systems")]
-[assembly: AssemblyProduct("FrontEnd")]
-[assembly: AssemblyCopyright("Copyright © EPAM Systems 2012")]
+[assembly: AssemblyCompany("Toolchain")]
+[assembly: AssemblyProduct("Basta")]
+[assembly: AssemblyCopyright("Copyright © Toolchain 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
