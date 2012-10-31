7f8c18a3645dd3a53e676d51f3d4d5fb32228b47
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-11-01 01:45:42 +0200

    Stripping down the code to speed up

--- Basta/Controllers/PastieController.cs
+++ Basta/Controllers/PastieController.cs
@@ -11,9 +11,9 @@ using Nancy;
 
 namespace Basta.Controllers
 {
-    public class NancyController : NancyModule
+    public class PastieController : NancyModule
     {
-        public NancyController()
+        public PastieController()
         {
             Get["/"] = parameters => 
             {
@@ -55,70 +55,6 @@ namespace Basta.Controllers
                     return View["Pastie/Details.cshtml", session.Load<Pastie>((string)parameters.id)];
                 }
             };
-        }
-    }
-
-    public class PastieController : Controller, IDataAccess
-    {
-        public IDocumentSession DocumentSession { get; set; }
-
-        //
-        // GET: /
-
-        public ActionResult Create()
-        {
-            var pastie = new Pastie();
-            return View(pastie);
-        }
-
-        //
-        // POST: /
-
-        [HttpPost]
-        [DataAccess]
-        public ActionResult Create(Pastie pastie)
-        {
-            while (this.DocumentSession.Load<Pastie>(pastie.Id) != null) 
-            {
-                pastie.RefreshId();
-            }
-
-            pastie.Creation = DateTime.UtcNow;
-            pastie.Expiration = DateTime.UtcNow.AddMonths(1);
-
-            this.DocumentSession.Store(pastie);
-            return RedirectToAction("Details", "Pastie", new { Id = pastie.Id }); 
-        }
-
-        //
-        // GET: /5de9y
-
-        [DataAccess]
-        public ActionResult Details(string id)
-        {
-            var pastie = this.DocumentSession.Load<Pastie>(id);
-
-            if (pastie == null || pastie.IsExpired() == true)
-            {
-                if (pastie != null)
-                {
-                    this.DocumentSession.Delete(pastie);
-                }
-
-                return RedirectToAction("Create");
-            }
-            else
-            {
-                return View(pastie);
-            }
-        }
-
-        //
-        // GET: /About
-
-        public ActionResult About()
-        {
-            return View();
         }
     }
 }
