20c9b2c0bb72bf53a4bce13e1de1daa7914d00d0
Alexey Shytikov <alexey.shytikov@gmail.com>
2012-11-01 12:33:45 +0200

    Draft implementation of the AJAX pastie creation.

---
+++ Basta/src/PastieModule.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Nancy;
+using Raven.Client;
+
+namespace Basta
+{
+    public class PastieModule : NancyModule
+    {
+        public PastieModule()
+        {
+            Get["/"] = parameters => 
+            {
+                return View["Pastie/Create.cshtml", new PastieModel()];
+            };
+
+            Get["/About"] = parameters =>
+            {
+                return View["Pastie/About.cshtml"];
+            };
+
+            Post["/Create"] = parameters =>
+            {
+                var pastie = new PastieModel
+                {
+                    Content = this.Request.Form.Content,
+                    Creation = DateTime.UtcNow,
+                    Expiration = DateTime.UtcNow.AddMonths(1)
+                };
+
+                using (var session = Storage.Instance.OpenSession())
+                {
+                    do
+                    {
+                        pastie.RefreshId();
+                    } while (session.Load<PastieModel>(pastie.Id) != null);
+
+                    session.Store(pastie);
+                    session.SaveChanges();
+                }
+
+                pastie.Content = null;
+                pastie.Creation = null;
+                pastie.Expiration = null;
+
+                // Sending only Id of the pastie created
+                return Response.AsJson<PastieModel>(pastie);
+            };
+
+            Get["/{id}"] = parameters =>
+            {
+                using (var session = Storage.Instance.OpenSession())
+                {
+                    return View["Pastie/Details.cshtml", session.Load<PastieModel>((string)parameters.id)];
+                }
+            };
+        }
+    }
+}
