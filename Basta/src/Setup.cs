d3b2e121ac5838ad5e60d2684716c811b6fde981
Alexey Shytikov <alexey.shytikov@gmail.com>
2013-01-03 17:41:06 +0200

    Preparing Install/Uninstall feature

---
+++ Basta/src/Setup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration.Install;
+
+namespace Basta
+{
+    class Setup : Installer
+    {
+    }
+}
