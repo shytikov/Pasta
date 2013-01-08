7ddabd3089cab2b44d6cff8bdf644f1a719527b7
Alexey Shytikov <alexey.shytikov@gmail.com>
2013-01-08 12:10:03 +0200

    Adding simple service installer

--- Basta/src/Setup.cs
+++ Basta/src/Setup.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
 using System.Configuration.Install;
+using System.ServiceProcess;
+using System.Configuration;
 
 namespace Basta
 {
+    [RunInstaller(true)]
     class Setup : Installer
     {
+        public Setup()
+        {
+            var processInstaller = new ServiceProcessInstaller();
+            var serviceInstaller = new ServiceInstaller();
+
+            // Set the privileges
+            processInstaller.Account = ServiceAccount.LocalSystem;
+
+            serviceInstaller.DisplayName = ConfigurationManager.AppSettings["name"];
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            // Must be the same as what was set in Program's constructor
+            serviceInstaller.ServiceName = ConfigurationManager.AppSettings["name"];
+
+            this.Installers.Add(processInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
     }
 }
